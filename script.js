32720b83c81dd23eaa712ea77215d0a4f800b831
kr1skyyy <kr1skyyy@abv.bg>
2019-12-06 17:42:41 +0200

    smaller payouts and lower chances of winning

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ $(document).ready(function () {
             'text-align': 'center'
 		}).show();
     }
+    function wild(chance){
+        return Math.floor(Math.random() * chance+1);
+    }
      var credits = 100, bet, win;
      var pos1,pos2,pos3;
-     var wildCard = 5;
+     var wildCard = 10;
+     var bonusGame;
     $(".button").on('click', function(e){
         e.preventDefault();
         $('.status').hide();
         bet = $("#bet").val();
-        console.log(bet);
         if (bet > credits)
             showMessage('Insufficient funds',5000);
         else{
@@ -22,55 +25,61 @@ $(document).ready(function () {
             else{
                 credits -= bet;
                 win=0;
+                $('#win').text("Win: 0");
                 pos1=Math.floor(Math.random() * 3);
                 pos2=Math.floor(Math.random() * 3);
                 pos3=Math.floor(Math.random() * 3);
 
-                if(pos1 === pos2 && pos2 === pos3 && credits < 50)
+                if(pos1 === pos2 && pos2 === pos3 && credits < 200)
                     pos3=Math.floor(Math.random() * 3); //decreasing chance of winning
                 $('#1').attr('src', pos1 +".png");
                 $('#2').attr('src', pos2 +".png");
                 $('#3').attr('src', pos3 +".png");
-                $("#credits").text("Credits: " + credits);
+                $("#credits").text("Credits: " + credits.toFixed(2));
                 if(pos1 === pos2 && pos2 === pos3){
-                    win=bet*10*(pos1+1);
-                    $("#win").text("Win: " + win);
-                    showMessage("You win : "+win, 5000);
+                    win=bet*3*(pos1+1);
+                    $("#win").text("Win: " + win.toFixed(2));
+                    showMessage("You win : "+win.toFixed(2), 5000);
                 }
                 else{
-                    if (Math.floor(Math.random() * 50 > 48)) {
-                        console.log(pos1,pos2,pos3);
+                    if(credits<200){
+                        bonusGame = wild(50);
+                    }
+                    else{
+                        bonusGame = wild(150);
+                    }
+                    console.log(bonusGame);
+                    if (bonusGame === 1){
                         if(pos1 === pos3){
                             $('#2').attr('src',"3.png");
-                            win = bet*10*(pos1+1)*wildCard;
-                            $("#win").text("Win: " + win);
-                            showMessage("Big win : "+win, 5000);
+                            win = bet*3*(pos1+1)*wildCard;
+                            $("#win").text("Win: " + win.toFixed(2));
+                            showMessage("Big win : "+win.toFixed(2), 5000);
                         }
-                       else if(pos1 === pos2){
+                    else if(pos1 === pos2){
                             $('#3').attr('src',"3.png");
-                            win = bet*10*(pos1+1)*wildCard;
-                            $("#win").text("Win: " + win);
-                            showMessage("Big win : "+win, 5000);
+                            win = bet*3*(pos1+1)*wildCard;
+                            $("#win").text("Win: " + win.toFixed(2));
+                            showMessage("Big win : "+win.toFixed(2), 5000);
                         } 
-                      else if(pos2 === pos3){
+                    else if(pos2 === pos3){
                             $('#1').attr('src',"3.png");
-                            win = bet*10*(pos2+1)*wildCard;
-                            $("#win").text("Win: " + win);
-                            showMessage("Big win : "+win, 5000);
+                            win = bet*3*(pos2+1)*wildCard;
+                            $("#win").text("Win: " + win.toFixed(2));
+                            showMessage("Big win : "+win.toFixed(2), 5000);
                         } 
-                      else  if(pos1 !== pos2 && pos1 !== pos3 && pos2 !== pos3){
+                    else  if(pos1 !== pos2 && pos1 !== pos3 && pos2 !== pos3){
                             $('#1').attr('src', "3.png");
                             $('#2').attr('src', "3.png");
                             $('#3').attr('src', "3.png");
                             win = bet*wildCard*wildCard*wildCard;
-                            $("#win").text("Win: " + win);
-                            showMessage("Huge win : "+win, 5000);
-                        
+                            $("#win").text("Win: " + win.toFixed(2));
+                            showMessage("Huge win : "+win.toFixed(2), 5000);
                     }
                 }
             }
             credits+=win;
-            $("#credits").text("Credits: " + credits);
+            $("#credits").text("Credits: " + credits.toFixed(2));
             }
         }
     });
